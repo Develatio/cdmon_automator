7f89e7c9da67c011a31e73cdac1cf5290ac5d28a
Alexander Nestorov <alexandernst@gmail.com>
2018-10-12 18:27:36 +0200

    Improve release script

--- make_release.sh
+++ make_release.sh
@@ -1,5 +1,15 @@
 python3 -m pip install --upgrade setuptools wheel
 python3 -m pip install --upgrade twine
 
+read -e -p "Did you create a git tag? (y/n)" resp
+if [ "$resp" != "y" ]; then
+    exit
+fi
+
+read -e -p "Did you update the 'version' in the setup.py? (y/n)" resp
+if [ "$resp" != "y" ]; then
+    exit
+fi
+
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
