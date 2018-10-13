48a979a2369c625efeec6c6790a62fc2133a9174
Alexander Nestorov <alexandernst@gmail.com>
2018-10-13 13:41:00 +0200

    Prepare for 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cdmon_automator",
-    version="0.0.6",
+    version="0.0.7",
     author="Develatio",
     author_email="contact@develat.io",
     description="Library for CRUD operations on cdmon.com",
